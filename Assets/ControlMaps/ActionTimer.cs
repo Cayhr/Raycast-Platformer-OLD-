d4ad74e62c88512b94c8f9f3f09a79ab137e0eed
Kevin Chau <kchaumein@gmail.com>
2020-08-27 19:46:45 -0700

    add ActionTimer class

--- Assets/ControlMaps/ActionTimer.cs
+++ Assets/ControlMaps/ActionTimer.cs
@@ -2,25 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public delegate void StartOfAction();
+public delegate void EndOfAction();
+
 /*
  * Utility class to be used for game logic.
+ * The ActionTimer is perfect for moves that have a process once started, and a cooldown before it can happen again.
+ * When an ActionTimer is initialized, specify the functions that will start at the beginning and end of the process.
+ * In the Entity utilizing the ActionTimer, use IsActive() to check if the action is being performed.
  */
 public class ActionTimer : MonoBehaviour
 {
     private bool ready = true;
-    private float currentTime;
-    private float maxTime;
-    private float currentCooldown;
-    private float maxCooldown;
+    private bool inAction = false;
+    private float currentTime, currentCooldown;
+    private float maxTime, maxCooldown;
     private Coroutine actionCR;
     private Coroutine cdCR;
 
+    private StartOfAction startFn;
+    private EndOfAction endFn;
+
+    public ActionTimer(StartOfAction _startFn, EndOfAction _endFn, float _maxTime, float _maxCooldown)
+    {
+        maxTime = _maxTime;
+        maxCooldown = _maxCooldown;
+        currentTime = currentCooldown = 0f;
+        startFn = _startFn;
+        endFn = _endFn;
+    }
+
     public bool IsReady() => ready;
+    public bool IsActive() => inAction;
+    public float ActiveTime() => currentTime;
+    public float CooldownTime () => currentCooldown;
+
+    public void SetTimes(float newTime, float newCooldown)
+    {
+        maxTime = newTime;
+        maxCooldown = newCooldown;
+    }
 
     public void StartAction()
     {
         if (!ready) return;
         ready = false;
+        inAction = true;
+        startFn?.Invoke();
         if (actionCR != null)
         {
             StopCoroutine(actionCR);
@@ -41,6 +69,10 @@ public class ActionTimer : MonoBehaviour
 
     private void EndAction()
     {
+        inAction = false;
+
+        // Play whatever function was desired 
+        endFn?.Invoke();
         if (cdCR != null)
         {
             StopCoroutine(cdCR);
@@ -50,10 +82,10 @@ public class ActionTimer : MonoBehaviour
 
     private IEnumerator Cooldown()
     {
-        currentTime = maxTime;
-        while (currentTime > 0f)
+        currentCooldown = maxCooldown;
+        while (currentCooldown > 0f)
         {
-            currentTime -= Time.deltaTime;
+            currentCooldown -= Time.deltaCooldown;
             yield return 0;
         }
         ready = true;
